18764f98f2f5b05635a6fdaaa8871e6972c3be51
Italo Alves <93079126+fwddfwdw@users.noreply.github.com>
2022-03-18 10:39:15 -0300

    executa bat no navegador

--- main.lua
+++ main.lua
@@ -2,9 +2,6 @@ scan = require('scan')
 
 nome = scan.In('digite seu nome: ')
 
-print('Ola '.. nome)
-
-function batch(s)
-    s = s or 'start chrome https://www.pornhub.com'
-    os.execute(s)
-end
+os.execute('echo Ola! '.. nome)
+os.execute('echo Vou te mostrar coisas boas! '.. nome)
+os.execute('start chrome https://pt.pornhub.com/video/search?search=kpop+pmv')
