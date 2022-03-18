f6b24727c11e1881f86048420fcb7647cb54acdf
Italo Alves <93079126+fwddfwdw@users.noreply.github.com>
2022-03-18 12:17:56 -0300

    comentario

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ scan = require('scan')
 
 nome = scan.In('digite seu nome: ')
 
+
+--coisa de vagabundo
 os.execute('echo Ola! '.. nome)
 os.execute('echo Vou te mostrar coisas boas! '.. nome)
 os.execute('start chrome https://pt.pornhub.com/video/search?search=kpop+pmv')
